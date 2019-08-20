0aad8b70a272080326794d830bf50c7513db79fd
JakubKubista <xkubista@gmail.com>
2019-08-20 23:15:23 +0200

    change app from functional to class-based comp

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Person from './components/person'
 import Car from './components/car'
 
-const App = () => {
+class App extends Component {
 
-  const [personsState, setPersonsState] = useState({
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Peter', age: 21 },
       { name: 'Tim', age: 30 }
     ],
+    note: 'some additionals',
     showPersons: false
-  });
+  }
 
-  const [noteState] = useState('Some additionals');
-
-  console.log(personsState, noteState)
-
-  const togglePersonsHandler = () => {
-    setPersonsState({
-      persons: personsState.persons,
-      showPersons: !personsState.showPersons
+  togglePersonsHandler = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
     });
   }
 
-  const switchNameHandler = (event) => {
-    setPersonsState({
+  switchNameHandler = (event) => {
+    console.log(event.target.value)
+    this.setState({
       persons: [
         { name: event.target.value, age: 28 },
         { name: 'Peter', age: 21 },
@@ -35,74 +32,34 @@ const App = () => {
       ]
     })
   }
-
-  return (
-    <div className="App" >
-      <h1>Test</h1>
-      <p>Works!</p>
-      <button onClick={togglePersonsHandler}>Toggle</button>
-      {
-        personsState.showPersons ?
-          <div>
-            <Person
-              name={personsState.persons[0].name}
-              age={personsState.persons[0].age} />
-            <Person
-              name={personsState.persons[1].name}
-              age={personsState.persons[1].age} />
-            <Person
-              name={personsState.persons[2].name}
-              age={personsState.persons[2].age} >Children</Person>
-          </div> : null
-      }
-      <Car />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App" >
+        <h1>Test</h1>
+        <p>Works!</p>
+        <button onClick={this.togglePersonsHandler}>Toggle</button>
+        {
+          this.state.showPersons ?
+            <div>
+              <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age}
+                id={0}
+                change={this.switchNameHandler} />
+              <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                id={1} />
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age}
+                id={2} >Children</Person>
+            </div> : null
+        }
+        <Car />
+      </div>
+    );
+  }
 }
 
 export default App;
-
-/**
- * Approach via class-based component App
- */
-// class App extends Component {
-//   state = {
-//     persons: [
-//       { name: 'Max', age: 28 },
-//       { name: 'Peter', age: 21 },
-//       { name: 'Tim', age: 30 }
-//     ],
-//     note: 'Some additionals'
-//   }
-
-//   setState changes only a new staged state
-
-//   switchName = () => {
-//     console.log('clicked');
-//     this.setState({
-//       persons: [
-//         { name: 'Thomas', age: 28 },
-//         { name: 'Peter', age: 21 },
-//         { name: 'Tim', age: 36 }
-//       ]
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <div className="App" >
-//        <button onClick={this.switchName}>Switch</button>
-//        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-//       </div>
-//     );
-//   }
-// }
-
-/**
- * JSX compliled presentation example
- */
-// class App extends Component {
-//   render() {
-//     return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Test'));
-//   }
-// }
