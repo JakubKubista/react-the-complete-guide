468933352ef599d8c88ec7084c7cc37dc4d7de9e
JakubKubista <xkubista@gmail.com>
2019-08-26 22:43:45 +0200

    Add styles for menu icon

--- src/components/layout/toolbar/toolbar.js
+++ src/components/layout/toolbar/toolbar.js
@@ -3,12 +3,22 @@ import classes from './toolbar.scss';
 import Logo from '../logo/logo';
 import Menu from '../menu/menu';
 
-const iconLink = "https://icon-library.net/images/white-hamburger-menu-icon/white-hamburger-menu-icon-24.jpg";
+const iconMenu = {
+  src: 'https://icon-library.net/images/white-hamburger-menu-icon/white-hamburger-menu-icon-24.jpg',
+  style: {
+    height: '64px',
+    marginTop: '6px'
+  },
+  alt: 'icon-menu'
+}
 
 const Toolbar = props => (
   <header className={classes.Toolbar}>
     <div onClick={props.clickDrawer}>
-      <img src={iconLink} height="64px" />
+      <img
+        src={iconMenu.src}
+        style={iconMenu.style}
+        alt={iconMenu.alt} />
     </div>
     <div className={classes.Logo}>
       <Logo />
