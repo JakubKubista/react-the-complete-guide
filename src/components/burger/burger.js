162b627f8a709026b0d9b701298d3c41476e6719
JakubKubista <xkubista@gmail.com>
2019-08-26 23:48:58 +0200

    Add PureComponent

--- src/components/burger/burger.js
+++ src/components/burger/burger.js
@@ -1,41 +1,42 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './burger.scss';
 import BurgerIngredient from './burger-ingredient/burger-ingredient';
 
-const Burger = props => {
-
-  // Transform object into array with keys, where
+// Component was called after Order now button (Pure not)
+class Burger extends PureComponent {
+  render() {
+      // Transform object into array with keys, where
   // each element of object should has nested
   // array element for year value of counter
-  let transformedIngredients = Object.keys(props.ingredients)
-    .map(ingredientKey => {
-      return [...Array(props.ingredients[ingredientKey])]
-        .map((_, index) => {
-          return <BurgerIngredient
-            key={ingredientKey + index}
-            type={ingredientKey} />
-        });
-    })
-    .flat()
-  // The same like a .flat(1):
-  // .reduce(
-  //   (arr, el) => {
-  //     return arr.concat(el)
-  //   }, []);
+  let transformedIngredients = Object.keys(this.props.ingredients)
+  .map(ingredientKey => {
+    return [...Array(this.props.ingredients[ingredientKey])]
+      .map((_, index) => {
+        return <BurgerIngredient
+          key={ingredientKey + index}
+          type={ingredientKey} />
+      });
+  })
+  .flat()
+// The same like a .flat(1):
+// .reduce(
+//   (arr, el) => {
+//     return arr.concat(el)
+//   }, []);
 
-  if (transformedIngredients.length === 0) {
-    transformedIngredients = <p>Please start adding ingredients.</p>;
-  }
-
-  return (
-    <div className={classes.Burger}>
+if (transformedIngredients.length === 0) {
+  transformedIngredients = <p>Please start adding ingredients.</p>;
+}
+    return (
+      <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
       {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
-  );
+    )
+  }
 };
 
 Burger.propTypes = {
