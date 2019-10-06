4a7de4765e2e9766023897084ac6875196dfb33a
JakubKubista <xkubista@gmail.com>
2019-10-06 23:23:00 +0200

    Init project

--- src/index.js
+++ src/index.js
@@ -1,39 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-// global error service handling
-
-const intRequest = axios.interceptors.request.use(request => {
-  console.log(request)
-  // edit configuration (request element)
-  return request;
-}, error => {
-  console.log(error);
-  return Promise.reject(error);
-});
-
-const intResponse = axios.interceptors.response.use(response => {
-  console.log(response)
-  return response;
-}, error => {
-  console.log(error);
-  return Promise.reject(error);
-});
-
-//Removing Interceptors
-[intRequest, intResponse].forEach(el => axios.interceptors.request.eject(el));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
