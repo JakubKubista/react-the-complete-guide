9a38527df2b6a3da1e216dd7846f06c8980a1126
JakubKubista <xkubista@gmail.com>
2019-11-02 21:13:22 +0100

    Add react-router-dom imports and routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import Layout from '../containers/layout/layout';
 import BurgerBuilder from './burger-builder/burger-builder';
 import Checkout from './checkout/checkout';
@@ -7,8 +9,8 @@ class App extends Component {
 
   render() {
     return <Layout >
-      <BurgerBuilder />
-      <Checkout />
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/" exact component={BurgerBuilder} />
     </Layout>;
   }
 }
