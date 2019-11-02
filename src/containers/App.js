58d9d725fb6d5a8fecc060c7008a5b24f508b758
JakubKubista <xkubista@gmail.com>
2019-11-02 21:05:05 +0100

    Add checkout container and checkout-summary

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Layout from '../containers/layout/layout';
 import BurgerBuilder from './burger-builder/burger-builder';
+import Checkout from './checkout/checkout';
 
 class App extends Component {
 
   render() {
     return <Layout >
       <BurgerBuilder />
+      <Checkout />
     </Layout>;
   }
 }
