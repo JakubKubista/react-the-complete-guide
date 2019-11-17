49e416b575a3a50bf68f669abc8ea4bc3df69779
JakubKubista <xkubista@gmail.com>
2019-11-17 20:11:08 +0100

    Change store via immutable update pattern

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,43 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
+        ...state,
         counter: state.counter + 1
       }
     case 'DECREMENT':
       return {
+        ...state,
         counter: state.counter - 1
       }
     case 'ADD':
       return {
+        ...state,
         counter: state.counter + action.value
       }
     case 'SUBTRACT':
       return {
+        ...state,
         counter: state.counter - action.value
+      }
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: state.counter})
+      }
+    case 'DELETE_RESULT':
+      // for case if id is index
+      // const newResults = [...state.results];
+      // newResults.splice(action.resultId, 1);
+      const newResults = state.results.filter(result => result.id !== action.resultId);
+      return {
+        ...state,
+        results: newResults
       }
     default:
       return state;
