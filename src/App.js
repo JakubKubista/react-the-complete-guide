b7ec6636331606f4fa5e333898a6b08a7a25c28b
JakubKubista <xkubista@gmail.com>
2019-11-23 23:47:52 +0100

    Init project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Persons from './containers/Persons';
 
 class App extends Component {
-
   render() {
     return (
-      <div className="App" >
-        <p>Hello World!</p>
+      <div className="App">
+        <ol>
+          <li>Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
+        </ol>
+        <Persons />
       </div>
     );
   }
