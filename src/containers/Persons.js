2d7525701d062da46ee010848ae7296897f4ebb3
JakubKubista <xkubista@gmail.com>
2019-11-24 22:36:41 +0100

    Create store at index, use store at container:
    - Add add and remove methods using redux at persons container
    - Render values from store
    - Create store and use reducer at index.js

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,44 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import * as actionTypes from '../store/actions';
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
-
     render () {
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
+                <AddPerson personAdded={this.props.onAddPerson} />
+                {this.props.persons.map(person => (
                     <Person
                         key={person.id}
                         name={person.name}
                         age={person.age}
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        clicked={() => this.props.onRemovePerson(person.id)}/>
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+const generateNewPerson = () => {
+    return {
+        id: Math.random(), // not really unique but good enough here!
+        name: 'Max',
+        age: Math.floor( Math.random() * 40 )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        persons: state.persons
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPerson: () => dispatch({type: actionTypes.ADD_PERSON, person: generateNewPerson()}),
+        onRemovePerson: (personId) => dispatch({type: actionTypes.REMOVE_PERSON, personId: personId})
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
