7cd6571358673de45ac761ccb7c32a11e8ca8275
JakubKubista <xkubista@gmail.com>
2019-12-01 21:02:21 +0100

    Add redux and react-redux packages + usage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import classes from './index.scss';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
+const reducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+})
+
+const store = createStore(reducer);
+
 const app = (
-  <BrowserRouter>
-    <App className={classes.body} />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App className={classes.body} />
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
