e6784424c4c52f586d6bdd4be2b6d5f4cf7d088b
JakubKubista <xkubista@gmail.com>
2019-12-08 15:22:52 +0100

    Add reducer actions for ingredients

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,11 +1,34 @@
 import * as actionTypes from '../actions/types';
 
 const initialState = {
+  ingredients: {
+    salar: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  },
+  price: 0,
   purchasing: false
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.INGREDIENT_ADD:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        }
+      }
+    case actionTypes.INGREDIENT_REMOVE:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
+      }
     case actionTypes.PURCHASING_ON:
       return {
         ...state,
