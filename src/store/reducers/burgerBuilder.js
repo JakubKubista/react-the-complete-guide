75d44f230a586d26c6e493135fa6f5dde4dc68c3
JakubKubista <xkubista@gmail.com>
2019-12-14 21:52:30 +0100

    Change reducer to leaner form

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../actions/types';
 import {INGREDIENT_PRICES} from '../../constants/burger';
+import { updateObject } from '../../utils/index';
 
 const initialState = {
   ingredients: {
@@ -12,36 +13,58 @@ const initialState = {
   purchasing: false
 }
 
+/* Ingredient helper */
+
+const ingredientAdd = (state, action) => {
+  const updatedStates = {
+    ingredients: {
+      ...state.ingredients,
+      [action.name]: state.ingredients[action.name] + 1
+    },
+    price: state.price + INGREDIENT_PRICES[action.name]
+  }
+
+  return updateObject(state, {...updatedStates});
+}
+
+const ingredientRemove = (state, action) => {
+  const updatedStates = {
+    ingredients: {
+      ...state.ingredients,
+      [action.name]: state.ingredients[action.name] - 1
+    },
+    price: state.price - INGREDIENT_PRICES[action.name]
+  }
+
+  return updateObject(state, {...updatedStates});
+}
+
+/* Purchasing helper */
+
+const purchasingOn = (state) => {
+  return updateObject(state, {purchasing: true});
+}
+
+const purchasingOff = (state, action) => {
+  return updateObject(state, {purchasing: false});
+}
+
+/* Reducer */
+
 const burgerBuilderReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.INGREDIENT_ADD:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.name]: state.ingredients[action.name] + 1
-        },
-        price: state.price + INGREDIENT_PRICES[action.name]
-      }
+      return ingredientAdd(state, action);
+
     case actionTypes.INGREDIENT_REMOVE:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.name]: state.ingredients[action.name] - 1
-        },
-        price: state.price - INGREDIENT_PRICES[action.name]
-      }
+      return ingredientRemove(state, action);
+
     case actionTypes.PURCHASING_ON:
-      return {
-        ...state,
-        purchasing: true
-      }
+      return purchasingOn(state, action);
+
     case actionTypes.PURCHASING_OFF:
-      return {
-        ...state,
-        purchasing: false
-      }
+      return purchasingOff(state, action);
+
     default:
       return state;
   }
