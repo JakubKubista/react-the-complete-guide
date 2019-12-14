2fd582d8084ca0c95c2465eea09e18b6479366c4
JakubKubista <xkubista@gmail.com>
2019-12-14 18:46:39 +0100

    Refactor logger

--- src/store/middlewares/logger.js
+++ src/store/middlewares/logger.js
@@ -1,10 +1,8 @@
-export const logger = store => {
-  return next => action => {
+export const logger = store => next => action => {
     console.log('[Middleware] Dispatching');
     console.log(action);
     const result = next(action);
     console.log('[Middleware] Next state');
     console.log(store.getState());
     return result;
-  }
 };
