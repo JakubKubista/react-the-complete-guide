4e8e424a7acc3e9267b4f935f0cdb40a00f4bedc
JakubKubista <xkubista@gmail.com>
2020-01-04 18:20:49 +0100

    Refactor form of login

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -15,24 +15,26 @@ class Login extends Component {
   render () {
     const formElementsArray = createArrayOfFormElements(this.state.loginForm);
 
-    const form = formElementsArray.map(formElement => (
-      <Input
-        key={formElement.id}
-        elementType={formElement.config.elementType}
-        elementConfig={formElement.config.elementConfig}
-        value={formElement.config.value}
-        invalid={!formElement.config.valid}
-        shouldValidate={formElement.config.validation}
-        touched={formElement.config.touched}
-        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-    ));
+    let form = (
+      <form>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+        ))}
+        <Button btnType="Success">{BUTTONS.submit}</Button>
+      </form>
+    );
 
     return (
       <div className={classes.DefaultForm}>
-        <form>
-          {form}
-          <Button btnType="Success">{BUTTONS.submit}</Button>
-        </form>
+        {form}
       </div>
     )
   }
