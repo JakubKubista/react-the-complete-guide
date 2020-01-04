8e56814d0e574b6b2a75a8bebff62dbd43c95c40
JakubKubista <xkubista@gmail.com>
2020-01-04 21:16:05 +0100

    Make api key as separate const

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,5 @@
+const API_KEY = 'AIzaSyBd_jAoZaKmOcxwKZ7bbBgFcox03OBuj3A';
+
 export const DATABASE = 'https://react-guide-burger.firebaseio.com/';
-export const SING_UP = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBd_jAoZaKmOcxwKZ7bbBgFcox03OBuj3A';
-export const SING_IN = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBd_jAoZaKmOcxwKZ7bbBgFcox03OBuj3A';
+export const SING_UP = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY;
+export const SING_IN = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + API_KEY;
