6cde5394b5a3bfcf60e5d02f9a938050c840d9eb
JakubKubista <xkubista@gmail.com>
2020-01-19 17:54:36 +0100

    Add test cases checking content of compnent

--- src/components/layout/menu/menu.test.js
+++ src/components/layout/menu/menu.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import { MENU_ITEMS } from '../../../constants/menu';
 import Menu from './menu';
 import MenuItem from './menu-item/menu-item';
 
@@ -23,5 +24,22 @@ describe('<Menu/>', () => {
     wrapper.setProps({isSignedIn: true});
 
     expect(wrapper.find(MenuItem)).toHaveLength(3);
+  });
+
+  it('should have <MenuItem /> with Sign In', () => {
+    expect(wrapper.contains(
+      <MenuItem link={MENU_ITEMS.auth.signIn.route} exact>
+        {MENU_ITEMS.auth.signIn.label}
+      </MenuItem>
+    )).toEqual(true);
+  });
+  it('should have <MenuItem /> with Sign Out', () => {
+    wrapper.setProps({isSignedIn: true});
+
+    expect(wrapper.contains(
+      <MenuItem link={MENU_ITEMS.auth.signOut.route} exact>
+        {MENU_ITEMS.auth.signOut.label}
+      </MenuItem>
+    )).toEqual(true);
   });
 });
