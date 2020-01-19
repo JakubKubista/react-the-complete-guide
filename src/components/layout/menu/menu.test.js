517714191b3a18c111f0e7d5ff1a6a279cc6b53a
JakubKubista <xkubista@gmail.com>
2020-01-19 17:55:12 +0100

    Add snapshot test for a Menu component

--- src/components/layout/menu/menu.test.js
+++ src/components/layout/menu/menu.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import {create} from 'react-test-renderer';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { BrowserRouter } from 'react-router-dom';
 
 import { MENU_ITEMS } from '../../../constants/menu';
 import Menu from './menu';
@@ -15,6 +16,20 @@ describe('<Menu/>', () => {
   beforeEach(() => {
     wrapper = shallow(<Menu />);
   })
+
+  it('should show wrapper via debug', () => {
+    console.log(wrapper.debug());
+  });
+
+  it('should match to snapshot', () => {
+    const renderer = create(
+      <BrowserRouter>
+        <Menu />
+      </BrowserRouter>
+    );
+
+    expect(renderer.toJSON()).toMatchSnapshot();
+  });
 
   it('should render 2x <MenuItem /> if isSignedIn is not true', () => {
     expect(wrapper.find(MenuItem)).toHaveLength(2);
