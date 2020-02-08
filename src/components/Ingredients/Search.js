4e000d0e694b5139830d5acb0736608f396c5d7c
JakubKubista <xkubista@gmail.com>
2020-02-08 01:35:41 +0100

    Update search input to correct way

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,28 +1,30 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { throttle } from 'lodash';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 import { loadIngredients } from '../../utils/services';
 
-const SEARCH_INPUT_THROTTLE_LENGTH = 1000;
+const SEARCH_INPUT_THROTTLE_LENGTH = 500;
 
 const Search = React.memo(props => {
   const {onLoadIngredients} = props;
   const [input, setInput] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = input.length === 0 ? '' : `?orderBy="title"&equalTo="${input}"`;
+    setTimeout(() => {
+      if (input === inputRef.current.value) {
 
-    loadIngredients(query).then((data) => {
-      onLoadIngredients(data);
-    });
-  }, [input, onLoadIngredients]);
+        const query = input.length === 0 ? '' : `?orderBy="title"&equalTo="${input}"`;
 
-  const onInputHandler = useCallback(throttle((value) => {
-    setInput(value);
-  }, SEARCH_INPUT_THROTTLE_LENGTH), []);
+        loadIngredients(query).then((data) => {
+          onLoadIngredients(data);
+        });
+      }
+    }, SEARCH_INPUT_THROTTLE_LENGTH)
+
+  }, [input, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -30,9 +32,10 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             valie={input}
-            onChange={event => onInputHandler(event.target.value)}
+            onChange={event => setInput(event.target.value)}
           />
         </div>
       </Card>
